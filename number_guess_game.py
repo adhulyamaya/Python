4736f2fd4211af2f86cf3e2ab028c7520891f6b9
adhulyamaya <adhulyamaya927@gmail.com>
2024-04-05 16:57:42 +0530

    number_guess game

--- number_guess_game.py
+++ number_guess_game.py
@@ -1,31 +1,31 @@
-# import random
+import random
 
-# top_of_range =input('Type your number ')
-# if top_of_range.isdigit():
-#   top_of_range = int(top_of_range)
+top_of_range =input('Type your number ')
+if top_of_range.isdigit():
+  top_of_range = int(top_of_range)
 
-#   if top_of_range<=0:
-#     print('write the number larger than 0 next time')
-#     quit()
+  if top_of_range<=0:
+    print('write the number larger than 0 next time')
+    quit()
 
-# else:
-#   print('please write a number ')
-#   quit()    
+else:
+  print('please write a number ')
+  quit()    
 
-# r = random.randint(0,top_of_range)
+r = random.randint(0,top_of_range)
 
-# while True:
-#   guess = input('make guess again')
-#   if guess.isdigit():
-#     guess =int(guess)
+while True:
+  guess = input('make guess again')
+  if guess.isdigit():
+    guess =int(guess)
 
-#   else:
-#     print('please enter a number next time')
+  else:
+    print('please enter a number next time')
 
 
-#   if guess == r:
-#       print('you got it')
-#       break
-#   else:
-#       print('wrong')
-#       continue
+  if guess == r:
+      print('you got it')
+      break
+  else:
+      print('wrong')
+      continue
